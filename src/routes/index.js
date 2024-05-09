c67433221e360872270affe1c53bef98a7f7c237
haykAbrahamya <abrahamyan.hayk.03@gmail.com>
2024-05-09 16:38:04 +0400

    feat: initial

---
+++ src/routes/index.js
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect } from 'react';
+import {
+	Routes as CoreRoutes,
+	Route,
+} from 'react-router-dom';
+
+import { AuthPage } from '../pages/auth';
+import { AuthContext } from 'store/auth';
+import {
+	Navigate,
+	useNavigate,
+} from 'react-router-dom/dist';
+import { PapersPage } from 'pages/papers';
+import { CoversPage } from 'pages/covers';
+import { TemplatesPage } from 'pages/templates';
+
+export const Routes = () => {
+	const [token] = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!token && !localStorage.getItem('token'))
+			navigate('/auth');
+	}, [token, navigate]);
+
+	return (
+		<CoreRoutes>
+			{!token && (
+				<Route path='/auth' element={<AuthPage />} />
+			)}
+
+			<Route
+				index
+				path='/papers'
+				element={<PapersPage />}
+			/>
+			<Route
+				index
+				path='/templates'
+				element={<TemplatesPage />}
+			/>
+			<Route
+				index
+				path='/covers'
+				element={<CoversPage />}
+			/>
+			<Route path='*' element={<Navigate to='/covers' />} />
+		</CoreRoutes>
+	);
+};
